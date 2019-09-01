ccd27504d231df47edf4f223ad348ffa27e0169b
GeraxRM <geraxrosendo@gmail.com>
2019-09-01 00:11:49 -0500

    Se agrego ID a header de contacto.php y nosotros.php, se configuraron estilos de header y se anadieron recursos de fondo

---
+++ nosotros.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ScalaNegocios</title>
+    <link rel="stylesheet" href="/SitioWeb/css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,700|Roboto:300,400,700&display=swap"
+        rel="stylesheet">
+    <script src="https://kit.fontawesome.com/d00b1f9a63.js"></script>
+</head>
+
+<body>
+    <header id="nosotros">
+    <?php
+        require_once 'navbar.php';
+    ?>
+
+        <div class="contenedor">
+            <div class="contenedor-title">
+                <div class="title">
+                    <h1>¿Quiénes somos?</h1>
+                    <h2>Somos una agencia de negocios para emprendedores, empresarios  y gobierno, que  <br /> quieran crear, desarrollar o mejorar sus empresas.</h2>
+                </div>
+            </div>
+        </div>
+    </header>
+    
+    <footer>
+    <?php
+        require_once 'footer.php';
+    ?>
+    </footer>
+
+    <!-- <script src="/js/main.js"></script> -->
+</body>
+
+</html>
