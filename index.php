4448aaa3df0dda2d14feb1c0afb808539a698ad9
ChemaCso <56057878+ChemaCso@users.noreply.github.com>
2019-10-16 11:39:12 -0500

    Update index.php
    ajustes direcciones

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 <html lang="es">
 
 <head>
-  <link rel="stylesheet" href="/SitioWeb/cssC/estilos.css">
-  <link rel="stylesheet" href="/SitioWeb/css/style.css">
+  <link rel="stylesheet" href="/cssC/estilos.css">
+  <link rel="stylesheet" href="/css/style.css">
   <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,700|Roboto:300,400,700&display=swap"
       rel="stylesheet">
   <script src="https://kit.fontawesome.com/d00b1f9a63.js"></script>
